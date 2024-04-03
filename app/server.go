7aa7570d232a660bd9f6319d71f7ef466fc6b4bf
Ilja Polanskis <ilja.polanskis@pearlconvert.no>
2024-04-04 00:08:22 +0300

    Second stage done

--- app/server.go
+++ app/server.go
@@ -5,11 +5,16 @@ import "net"
 func main() {
 	l, err := net.Listen("tcp", ":4221")
 	if err != nil {
-		panic(err)
+		return
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		panic(err)
+		return
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		return
 	}
 }
