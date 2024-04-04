728ce572a729e1fbed1d9d9372fec425c63321aa
Ilja Polanskis <ilja.polanskis@pearlconvert.no>
2024-04-04 16:41:41 +0300

    Refactor & fifth stage done

--- app/server.go
+++ app/server.go
@@ -7,6 +7,149 @@ import (
 	"strings"
 )
 
+type Response struct {
+	version       string
+	statusCode    int
+	statusMessage string
+	headers       map[string]string
+	content       string
+}
+
+func NewResponse() *Response {
+	return &Response{
+		version:    "HTTP/1.1",
+		statusCode: 200,
+		headers:    make(map[string]string),
+	}
+}
+
+func (r *Response) AddHeader(key, value string) {
+	r.headers[key] = value
+}
+
+func (r *Response) SetContent(content string, contentType string) {
+	if len(content) == 0 {
+		return
+	}
+
+	r.AddHeader("Content-Length", strconv.Itoa(len(content)))
+	r.AddHeader("Content-Type", contentType)
+	r.content = content
+}
+
+func (r *Response) SetStatus(statusCode int) {
+	r.statusCode = statusCode
+
+	// TODO: Add more status codes
+	switch statusCode {
+	case 200:
+		r.statusMessage = "OK"
+	case 404:
+		r.statusMessage = "Not Found"
+	}
+}
+
+func (r *Response) Write(conn net.Conn) {
+	sb := strings.Builder{}
+	sb.WriteString(r.version)
+	sb.WriteString(" ")
+	sb.WriteString(strconv.Itoa(r.statusCode))
+	sb.WriteString(" ")
+	sb.WriteString(r.statusMessage)
+	sb.WriteString("\r\n")
+
+	for key, value := range r.headers {
+		sb.WriteString(key)
+		sb.WriteString(": ")
+		sb.WriteString(value)
+		sb.WriteString("\r\n")
+	}
+
+	if _, ok := r.headers["Content-Length"]; ok {
+		sb.WriteString("\r\n")
+		sb.WriteString(r.content)
+	}
+
+	sb.WriteString("\r\n")
+
+	conn.Write([]byte(sb.String()))
+}
+
+type Request struct {
+	version string
+	method  string
+	path    string
+	headers map[string]string
+	content string
+}
+
+func NewRequest(conn net.Conn) *Request {
+	byteReader := bufio.NewReader(conn)
+
+	startLine, _, _ := byteReader.ReadLine()
+
+	headers := make(map[string]string)
+	for {
+		header, _, err := byteReader.ReadLine()
+		if err != nil || len(header) == 0 {
+			break
+		}
+
+		headerParts := strings.Split(string(header), ": ")
+		if len(headerParts) != 2 {
+			continue
+		}
+
+		headers[headerParts[0]] = headerParts[1]
+	}
+
+	startLineParts := strings.Split(string(startLine), " ")
+	if len(startLineParts) != 3 {
+		return nil
+	}
+
+	// If headers have Content-Length, read the content
+	var content string
+	if contentLength, ok := headers["Content-Length"]; ok {
+		contentLengthInt, err := strconv.Atoi(contentLength)
+		if err != nil {
+			return nil
+		}
+
+		contentBytes := make([]byte, contentLengthInt)
+		_, err = byteReader.Read(contentBytes)
+		if err != nil {
+			return nil
+		}
+
+		content = string(contentBytes)
+	}
+
+	return &Request{
+		version: startLineParts[2],
+		method:  startLineParts[0],
+		path:    startLineParts[1],
+		headers: headers,
+		content: content,
+	}
+}
+
+func (r *Request) Method() string {
+	return r.method
+}
+
+func (r *Request) Path() string {
+	return r.path
+}
+
+func (r *Request) Header(key string) string {
+	return r.headers[key]
+}
+
+func (r *Request) Content() string {
+	return r.content
+}
+
 func main() {
 	l, err := net.Listen("tcp", "localhost:4221")
 	if err != nil {
@@ -18,73 +161,39 @@ func main() {
 		return
 	}
 
-	var buf []byte
-	_, err = conn.Read(buf)
-	if err != nil {
-		return
-	}
+	request := NewRequest(conn)
 
-	byteReader := bufio.NewReader(conn)
-	startLine, _, err := byteReader.ReadLine()
-
-	var headers []byte
-	for {
-		header, _, err := byteReader.ReadLine()
-		if err != nil || len(header) == 0 {
-			break
-		}
-		headers = append(headers, header...)
-		headers = append(headers, "\r\n"...)
-	}
-
-	startLineParts := strings.Split(string(startLine), " ")
-	if len(startLineParts) != 3 {
-		return
-	}
-
-	if startLineParts[0] == "GET" {
+	if request.Method() == "GET" {
 		switch {
-		case startLineParts[1] == "/":
+		case request.Path() == "/":
+
 			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 			if err != nil {
 				return
 			}
 			return
-		case strings.HasPrefix(startLineParts[1], "/echo/"):
-			content := strings.TrimPrefix(startLineParts[1], "/echo/")
+		case strings.HasPrefix(request.path, "/echo/"):
+			content := strings.TrimPrefix(request.Path(), "/echo/")
 
-			//HTTP/1.1 200 OK
-			//Content-Type: text/plain
-			//Content-Length: 3
+			response := NewResponse()
+			response.SetStatus(200)
+			response.SetContent(content, "text/plain")
+			response.Write(conn)
 
-			//abc
+			return
+		case request.Path() == "/user-agent":
+			userAgent := request.Header("User-Agent")
 
-			var reponse []byte
-			addHeaders(&reponse, "HTTP/1.1 200 OK")
-			addHeaders(&reponse, "Content-Type: text/plain")
-			addHeaders(&reponse, "Content-Length: "+strconv.Itoa(len(content)))
-			addContent(&reponse, content)
+			response := NewResponse()
+			response.SetStatus(200)
+			response.SetContent(userAgent, "text/plain")
+			response.Write(conn)
 
-			_, err = conn.Write(reponse)
-			if err != nil {
-				return
-			}
 			return
 		}
 	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	if err != nil {
-		return
-	}
-}
-
-func addHeaders(headers *[]byte, header string) {
-	*headers = append(*headers, header...)
-	*headers = append(*headers, "\r\n"...)
-}
-
-func addContent(content *[]byte, data string) {
-	*content = append(*content, "\r\n"...)
-	*content = append(*content, data...)
+	response := NewResponse()
+	response.SetStatus(404)
+	response.Write(conn)
 }
